d035f4d63cbb43ef6f86443819d3f3257f3f65a7
hiqbal-zing <husan.iqbal@zing.dev>
2024-01-18 11:11:25 +0000

    feat: format app settings composite action

---
+++ azure/format-app-settings/format-app-settings.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+appSettingsEnv=$1
+stickySettings=$2
+encrypt=$3
+encryptionPassword=$4
+encryptionFlags=$5
+
+IFS=',' read -ra settings <<< "$stickySettings"
+stickySettingsList=$(jq -n '$ARGS.positional' --args "${settings[@]}")
+
+settingsOutput=[]
+while IFS='=' read -r key value || [[ -n "$key" ]]; do
+  stickyIndex=$(echo "$stickySettingsList" | jq --arg NAME "$key" 'index($NAME) // empty')
+  [[ -z "$stickyIndex" ]] && slotSetting="false" || slotSetting="true"
+
+  settingObj="{
+    \"name\": \"$key\",
+    \"value\": \"$value\",
+    \"slotSetting\": $slotSetting
+  }"
+
+  settingsOutput=$(echo "$settingsOutput" | jq --argjson OBJ "$settingObj" '. += [$OBJ]')
+done <<< "$appSettingsEnv"
+
+if [ "$encrypt" == "true" ]; then
+  # shellcheck disable=SC2086
+  encryptedSettings=$(echo -n "$settingsOutput" |
+    openssl enc $encryptionFlags -k "$encryptionPassword" |
+    base64
+  )
+  settingsOutput="$encryptedSettings"
+fi
+
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "APP_SETTINGS<<$EOF" >> "$GITHUB_OUTPUT"
+echo -e "$settingsOutput" >> "$GITHUB_OUTPUT"
+echo "$EOF" >> "$GITHUB_OUTPUT"
