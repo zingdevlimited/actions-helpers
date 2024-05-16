575f9be4986cf3bff1ae2e6e5c74a0c081a5dd84
hiqbal-zing <husan.iqbal@zing.dev>
2024-05-16 15:57:17 +0100

    refactor: use js proxy backend for terraform init

---
+++ build-now/terraform-init/backend-proxy.js
@@ -0,0 +1,202 @@
+const {
+  INPUT_PLUGIN_NAME,
+  INPUT_TWILIO_API_KEY,
+  INPUT_TWILIO_API_SECRET,
+  INPUT_SYNC_SERVICE_SID,
+  INPUT_SYNC_MAP_NAME,
+  INPUT_BACKEND_PROXY_PORT
+} = process.env;
+
+const { createServer } = require("http");
+const { request } = require("https");
+
+const asyncRequest = async (requestOptions, data = undefined) =>
+  new Promise((resolve, reject) => {
+    const req = request(requestOptions, (res) => {
+      res.setEncoding("utf8");
+      let responseBody = "";
+
+      res.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+
+      res.on("end", () => {
+        const statusCode = res.statusCode ?? 500;
+        resolve({
+          body: responseBody,
+          status: statusCode,
+          ok: statusCode >= 200 && statusCode < 300,
+        });
+      });
+    });
+
+    req.on("error", (err) => {
+      reject(err);
+    });
+
+    if (data) {
+      req.write(data);
+    }
+    req.end();
+  });
+
+const mapBasePath = `/v1/Services/${INPUT_SYNC_SERVICE_SID}/Maps/${encodeURIComponent(INPUT_SYNC_MAP_NAME)}`;
+
+const getStateFile = async () => {
+  const getResponse = await asyncRequest(
+    {
+      method: "GET",
+      username: INPUT_TWILIO_API_KEY,
+      password: INPUT_TWILIO_API_SECRET,
+      hostname: "sync.twilio.com",
+      path: `${mapBasePath}/Items/${encodeURIComponent(INPUT_PLUGIN_NAME)}`
+    }
+  );
+
+  return getResponse;
+}
+
+const updateStateFile = async (data) => {
+  let mapExists = true;
+  const mapFetch = await asyncRequest(
+    {
+      method: "GET",
+      username: INPUT_TWILIO_API_KEY,
+      password: INPUT_TWILIO_API_SECRET,
+      hostname: "sync.twilio.com",
+      path: mapBasePath
+    }
+  );
+
+  if (mapFetch.status === 404) {
+    mapExists = false;
+    const mapCreateBody = `UniqueName=${encodeURIComponent(INPUT_SYNC_MAP_NAME)}`;
+    await asyncRequest(
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+          "Content-Length": Buffer.byteLength(mapCreateBody)
+        },
+        username: INPUT_TWILIO_API_KEY,
+        password: INPUT_TWILIO_API_SECRET,
+        hostname: "sync.twilio.com",
+        path: `/v1/Services/${INPUT_SYNC_SERVICE_SID}/Maps`
+      },
+      mapCreateBody
+    );
+  } else if (!mapFetch.ok) {
+    return false;
+  }
+
+  let mapItemNotFound = false;
+  if (mapExists) {
+    const mapItemUpdateBody = `Data=${encodeURIComponent(data)}`;
+    const mapItemUpdate = await asyncRequest(
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+          "Content-Length": Buffer.byteLength(mapItemUpdateBody)
+        },
+        username: INPUT_TWILIO_API_KEY,
+        password: INPUT_TWILIO_API_SECRET,
+        hostname: "sync.twilio.com",
+        path: `${mapBasePath}/Items/${INPUT_PLUGIN_NAME}`
+      },
+      mapItemUpdateBody
+    );
+
+    if (mapItemUpdate.status === 404) {
+      mapItemNotFound = true;
+    } else if (!mapItemUpdate.ok) {
+      return false;
+    }
+  }
+
+  if (!mapExists || mapItemNotFound) {
+    const mapItemCreateBody = `UniqueName=${encodeURIComponent(INPUT_PLUGIN_NAME)}&Data=${encodeURIComponent(data)}`;
+    const mapItemCreate = await asyncRequest(
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "x-www-form-urlencoded",
+          "Content-Length": Buffer.byteLength(mapItemCreateBody)
+        },
+        username: INPUT_TWILIO_API_KEY,
+        password: INPUT_TWILIO_API_SECRET,
+        hostname: "sync.twilio.com",
+        path: `${mapBasePath}/Items`
+      },
+      mapItemCreateBody
+    );
+    if (!mapItemCreate.ok) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const server = createServer(async (req, res) => {
+
+  console.log(`${req.method} ${req.url}`);
+  console.log(req.headers);
+  switch (req.method) {
+    case "GET":
+      if (req.url === "/ping") {
+        res.setHeader("Content-Type", "text/plain");
+        res.writeHead(200);
+        res.write("pong", "utf8");
+        res.end();
+      } else if (req.url === "/exit") {
+        res.setHeader("Content-Type", "text/plain");
+        res.writeHead(200);
+        res.write("Closing server...");
+        res.end(undefined, () => server.close());
+      } else {
+        const stateFileResponse = await getStateFile();
+
+        if (stateFileResponse.ok) {
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.write(stateFileResponse.body);
+          res.end();
+        } else {
+          res.writeHead(stateFileResponse.status);
+          res.end();
+        }
+      }
+      break;
+    case "POST":
+      let data = "";
+      req.on("data", chunk => {
+        data += chunk;
+      });
+
+      req.on("end", () => {
+        try {
+          const requestData = JSON.parse(data);
+
+          const updateResult = updateStateFile(requestData);
+
+          if (!updateResult) {
+            res.writeHead(500);
+            res.end();
+          } else {
+            res.writeHead(200);
+            res.setHeader("Content-Type", "application/json");
+            res.write(data);
+            res.end();
+          }
+        } catch (e) {
+          res.writeHead(400);
+          res.end("Invalid JSON");
+        }
+      });
+      break;
+  }
+});
+
+console.log(`Listening on ${INPUT_BACKEND_PROXY_PORT}...`);
+server.listen(INPUT_BACKEND_PROXY_PORT);
---
+++ build-now/terraform-init/backend-proxy/main.js
@@ -0,0 +1,42 @@
+const { spawn, execSync } = require("child_process");
+const { get } = require("http");
+const { exit } = require("process");
+
+const { INPUT_BACKEND_PROXY_PORT, INPUT_ACTION_PATH } = process.env;
+
+spawn("node", ["backend-proxy.js"], {
+  stdio: "ignore",
+  detached: true
+}, {
+  cwd: INPUT_ACTION_PATH
+}).unref();
+
+setTimeout(() => {
+  console.log(`Calling http://localhost:${INPUT_BACKEND_PROXY_PORT}/ping...`);
+  const req = get(`http://localhost:${INPUT_BACKEND_PROXY_PORT}/ping`, {
+    timeout: 5000
+  }, (res) => {
+    if (res.statusCode !== 200) {
+      console.log("::error::Ping to localhost failed");
+      exit(1);
+    }
+    console.log("Received 200 OK");
+
+    // const initConfig = `-backend-config="address=http://localhost:${INPUT_BACKEND_PROXY_PORT}"`
+    // execSync(`terraform init ${initConfig}`, {
+    //   stdio: "inherit",
+    //   cwd: INPUT_TERRAFORM_DIRECTORY
+    // });
+
+    // execSync("terraform validate", {
+    //   stdio: "inherit",
+    //   cwd: INPUT_TERRAFORM_DIRECTORY
+    // });
+  });
+
+  req.on("timeout", () => {
+    req.destroy();
+    console.log("::error::Ping to localhost timed out");
+    exit(1);
+  })
+}, 1000); // Delay to allow server startup
