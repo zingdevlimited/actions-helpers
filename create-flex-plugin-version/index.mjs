9c867505f5d9f69e607f0b194de453cc4dd2865d
hiqbal-zing <husan.iqbal@zing.dev>
2025-07-22 11:18:26 +0100

    fix: throw message + status

--- create-flex-plugin-version/index.mjs
+++ create-flex-plugin-version/index.mjs
@@ -92,7 +92,7 @@ const asyncTwilioRequest = async (
 
     const ok = req.status >= 200 && req.status < 300;
     if (!ok) {
-      throw new Error(`Error Response: ${await req.text()}`);
+      throw { message: await req.text(), status: req.status };
     }
 
     const responseBody = await req.json();
