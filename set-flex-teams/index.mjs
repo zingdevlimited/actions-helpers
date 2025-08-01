1aeada6153093d3dd966c1f9ae010180f3dfdb7c
np <nitya.powar@zing.dev>
2025-08-01 14:15:19 +0100

    feat: Added the action helper for setting teams on flex

---
+++ set-flex-teams/index.mjs
@@ -0,0 +1,147 @@
+import { appendFileSync } from "fs";
+import { readFile } from "fs/promises";
+
+const {
+  TWILIO_API_KEY,
+  TWILIO_API_SECRET,
+  CONFIG_PATH,
+  GITHUB_OUTPUT
+} = process.env;
+
+if (!TWILIO_API_KEY || !TWILIO_API_SECRET || !CONFIG_PATH) {
+  console.error("TWILIO_API_SECRET or TWILIO_API_KEY missing or CONFIG_PATH");
+  process.exit(-1);
+}
+
+const jsonText = await readFile(CONFIG_PATH, "utf-8");
+const requiredTeams = JSON.parse(jsonText);
+const BASE_URL = "https://flex-api.twilio.com/v1";
+const MAX_RETRY_COUNT = 3;
+const BASE_DELAY_MS = 2000;
+
+/**
+ * Generic Twilio request handler with retry logic.
+ */
+const asyncTwilioRequest = async (
+  url,
+  method,
+  bodyParams = undefined,
+  retryNumber = 0
+) => {
+  try {
+    console.log(`::debug::Request: ${method} ${url}`);
+    const headers = {
+      Authorization:
+        "Basic " +
+        Buffer.from(`${TWILIO_API_KEY}:${TWILIO_API_SECRET}`).toString("base64"),
+    };
+
+    let body = undefined;
+    if (bodyParams) {
+      const undefinedParams = [];
+      for (const [key, value] of Object.entries(bodyParams)) {
+        if (value === undefined) {
+          undefinedParams.push(key);
+        }
+      }
+      for (const key of undefinedParams) {
+        delete bodyParams[key];
+      }
+      body = new URLSearchParams(bodyParams).toString();
+    }
+
+    if (method === "POST") {
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+      headers["Content-Length"] = Buffer.byteLength(body ?? "");
+    }
+    
+    const req = await fetch(url, { method, headers, body });
+
+    if (req.status === 429) {
+      if (retryNumber >= MAX_RETRY_COUNT) {
+        throw new Error("Exceeded retry attempts after 429 errors");
+      }
+      const retryDelay = BASE_DELAY_MS * 2 ** retryNumber;
+      console.log(`::debug::Rate-limit hit, retrying in ${retryDelay} ms...`);
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+      return asyncTwilioRequest(url, method, bodyParams, retryNumber + 1);
+    }
+
+    console.log(`::debug::Status: ${req.status} ${req.statusText}`);
+
+    const ok = req.status >= 200 && req.status < 300;
+    if (!ok) {
+      throw new Error(`Error Response: ${await req.text()}`);
+    }
+
+    const responseBody = await req.json();
+
+    return {
+      body: responseBody,
+      status: req.status,
+      ok,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+  const instance = await asyncTwilioRequest(`${BASE_URL}/Configuration`, "GET");
+
+  if (!instance.body.flex_instance_sid) {
+    console.error("Flex instance sid not found");
+    process.exit(-1);
+  }
+
+  const instanceSid = instance.body.flex_instance_sid;
+  const levels = [3, 2, 1];
+
+  const teamsJson = await asyncTwilioRequest(
+    `${BASE_URL}/Instances/${instanceSid}/Teams?PageSize=1000`,
+    "GET"
+  );
+
+  const fetchedTeams = teamsJson.body.teams;
+  let createdCount = 0;
+
+  for (const level of levels) {
+    const teamsAtLevel = requiredTeams.filter((team) => team.level === level);
+
+    for (const requiredTeam of teamsAtLevel) {
+      const exists = fetchedTeams.some(
+        (fetchedTeam) => fetchedTeam.friendly_name === requiredTeam.friendlyName
+      );
+      if (!exists) {
+        const body = {
+          FriendlyName: requiredTeam.friendlyName,
+          Description: requiredTeam.description,
+          Level: level,
+        };
+
+        if (level < 3 && requiredTeam.parentTeam) {
+          const parentTeam = fetchedTeams.find(
+            (team) => team.friendly_name === requiredTeam.parentTeam
+          );
+          if (parentTeam) {
+            body["ParentTeamSid"] = parentTeam.team_sid;
+          }
+        }
+
+        const createdTeam = await asyncTwilioRequest(
+          `${BASE_URL}/Instances/${instanceSid}/Teams`,
+          "POST",
+          // @ts-ignore
+          body
+        );
+
+        fetchedTeams.push(createdTeam.body);
+        createdCount++;
+      }
+    }
+  }
+
+if (GITHUB_OUTPUT) {
+  appendFileSync(GITHUB_OUTPUT, `teams_created=${createdCount}\n`, "utf-8");
+}
+
+process.exit(0)
